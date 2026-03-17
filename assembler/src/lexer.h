ab96bafa0314d044f85d47572f5d3fd3c5979198
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2026-03-17 14:46:44 +0100

    feat(lexer): implement initial desing

--- assembler/src/lexer.h
+++ assembler/src/lexer.h
@@ -73,11 +73,6 @@
 typedef enum Token_Kind
 {
 	Token_Kind__None = 0,
-	Token_Kind__Identifier,
-
-	Token_Kind__Number_Literal,
-	Token_Kind__Char_Literal,
-	Token_Kind__String_Literal,
 
 	Token_Kind__Dot,
 	Token_Kind__Comma,
@@ -85,23 +80,47 @@ typedef enum Token_Kind
 
 	Token_Kind__Left_Parenthesis,
 	Token_Kind__Right_Parenthesis,
+
 	Token_Kind__Plus,
 	Token_Kind__Minus,
 	Token_Kind__Star,
 	Token_Kind__Slash,
+
 	Token_Kind__Pipe,
 	Token_Kind__Ampersand,
 	Token_Kind__Tilde,
-	Token_Kind__Shift_Left,
-	Token_Kind__Shift_Right,
 	Token_Kind__Caret,
 
+	Token_Kind__Percentage,
 	Token_Kind__Newline,
+
+	Token_Kind__Major,
+	Token_Kind__Minor,
+	Token_Kind__Shift_Left,
+	Token_Kind__Shift_Right,
+
+	Token_Kind__Char_Literal,
+	Token_Kind__String_Literal,
+
+	Token_Kind__Identifier,
+	Token_Kind__Number_Literal,
+
 	Token_Kind__EOF,
 
 	Token_Kind__COUNT
 }
 Token_Kind;
+
+typedef enum Lexing_Error_Kind
+{
+	Lexer_Error_Kind__None,
+	Lexer_Error_Kind__String_Multiline_Unsupported,
+
+	Lexer_Error_Kind__COUNT,
+
+
+}
+Lexer_Error_Kind;
 
 typedef enum Escaping_Stage
 {
@@ -123,8 +142,10 @@ struct Token_Array
 	Token_Kind *tokens;
 	U32         count;
 
-	U32	    row_current;
-	U32         column_current;
+	U32	    row_index_current;
+	U32         column_index_current;
+
+	Lexer_Error_Kind error;
 };
 
 internal B32
@@ -156,169 +177,209 @@ LE_String8_get(String8 *string, U64 index)
 internal Token_Array
 tokenize(String8 *input, Arena *arena)
 {
-	U32 row_index = 0;
+	U32 row_index    = 0;
 	U32 column_index = 0;
-	U32 token_index = 0;
+	U32 token_index  = 0;
 
-	U8 *input_begin = input->data;
-	U8 *input_end   = input->data + input->count - 1;
 	U8 *input_data  = input->data;
 	U64 input_count = input->count;
 
-	Lexer_Error_Kind error = Lexer_Error_Kind__None;
+	Lexer_Error_Kind error = 0;
 
-	// We overastimate using the file size
+	// We overastimate using the file size. Consider doing at the start of the program and not here.
 	U32 *positions = Arena_push_array_m(arena, U32, input_count);
 	U32 *sizes     = Arena_push_array_m(arena, U32, input_count);
-	U32 *sizes     = Arena_push_array_m(arena, Token_Kind, input_count);
+	U32 *tokens    = Arena_push_array_m(arena, Token_Kind, input_count);
 
 	Token_Kind token_kind = Token_Kind__None;
 
 	U64 index = 0;
 	for (;;)
 	{
-		if (index < input_count)
-		{
-			break;
-		}
+		U64 index_before = index;
 
+		// NOTE: should maintain the logical order of Token_Kind enumeration.
 		switch (input_data[index])
 		{
-			// TODO: add other symbols.
-			// NOTE: no multi-line comment support (yet).
-			case '#':
-				while (input_data[index] != '\n' && index < input_count)
-				{
-					index +=1;
-				}
-				break;
-			case '\n':
-				token_kind = Token_Kind__Newline;
-				break;
-			case ',':
-				token_kind = Token_Kind__Comma;
-				break;
-			case '.':
-				token_kind = Token_Kind__Dot;
-				break;
-			case ':':
-				token_kind = Token_Kind__Colon;
-				break;
-			case '(':
-				token_kind = Token_Kind__Left_Parenthesis;
-				break;
-			case ')':
-				token_kind = Token_Kind__Right_Parenthesis;
-				break;
-			case '+':
-				token_kind = Token_Kind__Plus;
-				break;
-			case '-':
-				token_kind = Token_Kind__Minus;
-				break;
-			case '>':
-				U8 *next = LE_String8_get(index + 1);
-				if (next && *next =
-				break;
-			// Maybe these two below are not needed?
-			case ' ':
-			case '\t'
-				break;
-			case '\'':
-			case '\"':
-				U8 character_quote = input_data[index];
-				U64 index_before = index;
+		case ' ' : { index += 1; } break;
+		case '\t': { index += 1; } break;
+
+		// NOTE: no multi-line comment support (yet).
+		case '#':
+		{
+			B32 break_should = 0;
+			for (;;)
+			{
 				index += 1;
 
-				// We only check for ending quotes: handling of bytes values and parsing of
-				// escapes is done at a later stage.
-				U8  escaping_started   = 0;
-				U8  quote_ending_found = 0;
-				B32 break_should       = 0;
+				break_should = input_data[index] != '\n' || index >= input_count;
+				if (break_should) { break; }
+			}
+
+		} break;
+
+		case '.' : { token_kind = Token_Kind__Dot;               index += 1; } break;
+		case ',' : { token_kind = Token_Kind__Comma;             index += 1; } break;
+		case ':' : { token_kind = Token_Kind__Colon;             index += 1; } break;
+
+		case '(' : { token_kind = Token_Kind__Left_Parenthesis;  index += 1; } break;
+		case ')' : { token_kind = Token_Kind__Right_Parenthesis; index += 1; } break;
+
+		case '+' : { token_kind = Token_Kind__Plus;              index += 1; } break;
+		case '-' : { token_kind = Token_Kind__Minus;             index += 1; } break;
+		case '*' : { token_kind = Token_Kind__Star;              index += 1; } break;
+		case '/' : { token_kind = Token_Kind__Slash;             index += 1; } break;
+
+		case '%' : { token_kind = Token_Kind__Percentage;        index += 1; } break;
+		case '\n': { token_kind = Token_Kind__Newline;           index += 1; } break;
+
+		case '>':
+		{
+			index += 1;
+			U8 *next = LE_String8_get(index);
+			if (next && *next == '>')
+			{
+				index += 1;
+				token_kind = Token_Kind__Shift_Right;
+			}
+			else
+			{
+				token_kind = Token_Kind__Major;
+			}
+		} break;
+		case '<':
+		{
+			index += 1;
+			U8 *next = LE_String8_get(index);
+			if (next && *next == '<')
+			{
+				index += 1;
+				token_kind = Token_Kind__Shift_Left;
+			}
+			else
+			{
+				token_kind = Token_Kind__Minor;
+			}
+		} break;
+
+		case '\'':
+		case '\"':
+		{
+			U8  character_quote = input_data[index];
+
+			// We only check for ending quotes: handling of bytes values and parsing of
+			// escapes is done at a later stage.
+			U8  escaping_started   = 0;
+			U8  quote_ending_found = 0;
+			B32 break_should       = 0;
+			for (;;)
+			{
+				index += 1;
+				U8  character = input_data[index];
+				B32 backslash_is = character == '\\';
+
+				if (escaping_started)
+				{
+					escaping_started = 0;
+				}
+				else if (character == character_quote)
+				{
+					quote_ending_found = 1;
+				}
+				else if (character == '\n')
+				{
+					error = Lexer_Error_Kind__String_Multiline_Unsupported;
+				}
+
+				break_should = quote_ending_found || error || index >= index_count;
+				if (break_should) { break; }
+
+			}
+
+			if (character_quote == '\"')
+			{
+				token_kind = Token_Kind__String_Literal;
+			}
+			else if (character_quote = '\'')
+			{
+				token_kind = Token_Kind__Char_Literal;
+			}
+			else
+			{
+				assert_always_m(0 && "unreachable");
+			}
+
+		} break;
+		default:
+		{
+			if (LE_U8_indentifier_is(input_data[index]))
+			{
+				B32 break_should = 0;
+				index += 1;
 				for (;;)
 				{
-					U8  character = input_data[index];
-					B32 backslash_is = character == '\\';
-
-					if (escaping_started)
+					B32 identifier_is = LE_U8_identifier_is(input_data[index]);
+					if (identifier_is)
 					{
-						escaping_started = 0;
-					}
-					else if (character == character_quote)
-					{
-						quote_ending_found = 1;
-					}
-					else if (character == '\n')
-					{
-						error = 1;
 					}
 
-					break_should = quote_ending_found || error;
-
-					if (!error)
-					{
-						index += 1
-					}
-
+					index += 1;
+					break_should = !identifier_is || index >= input_count;
 					if (break_should)
 					{
 						break;
 					}
 				}
+			}
 
-				if (character_quote == '\"')
+			// TODO: support float (hex float?), literal hex, literal octal, literal binary.
+			if (U8_ascii_digit_is(input_data[index])
+			{
+				index += 1;
+				for (;;)
 				{
-					token_kind = Token_Kind__String_Literal;
-				}
-				else if (character_quote = '\'')
-				{
-					token_kind = Token_Kind__Char_Literal;
-				}
-				else
-				{
-					assert_always_m(0 && "unreachable");
-				}
-
-				break;
-			default:
-				if (LE_U8_indentifier_is(input_data[index]))
-				{
-					U64 index_before = index;
-					index += 1;
-					for (;;)
+					if (U8_ascii_digit_is(input_data[index])
 					{
-						if (character_identifier_is(input_data[index])) break;
-						if (index < input_count) break;
-
 						index += 1;
 					}
-				}
-
-				// TODO: support float (hex float?), literal hex, literal octal, literal binary.
-				if (U8_ascii_digit_is(input_data[index])
-				{
-					U64 index_before = index;
-					index += 1;
-					for (;;)
+					else
 					{
-						if (U8_ascii_digit_is(input_data[index])
-						{
-							index += 1;
-						}
-						else
-						{
-							break;
-						}
+						break;
 					}
 				}
-				break;
-		} // switch
-
-		if (error_found)
-		{
-
+			}
+		} break;
 		}
-	} // for
+
+		assert_always_m(index_before < index && "infinite loop edge case");
+
+		if (token_kind != 0)
+		{
+			// Update phase
+			positions[token_index] = index_before;
+			tokens[token_index] = token_kind;
+			sizes[token_index] = index - index_before;
+			token_index += 1;
+		}
+
+		B32 break_should = error || index >= index_count;
+		if (break_should) { break; }
+	}
+
+	Token_Array token_array =
+	{
+		.positions            = positions,
+		.sizes                = sizes,
+		.tokens               = tokens,
+		.count                = token_index,
+
+		.row_index_current    = row_index_current,
+		.column_index_current = column_index_current,
+
+		.error                = error
+	};
+
+	return token_array;
 }
 
 // If a dot is found, peek the next character and check if it's a letter, if not we haven't found a valid identifier
